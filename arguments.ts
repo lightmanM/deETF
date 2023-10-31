1b1bd23f11969a1633e447ad9a17624d109e17b8
LightmanM <36053940+lightmanM@users.noreply.github.com>
2023-10-30 22:45:46 -0700

    Update arguments.ts

--- arguments.ts
+++ arguments.ts
@@ -1,6 +1,6 @@
 module.exports = [
     ["0x04a3944CCb7234FbE26111bf8684f33615e71e04", "0xC8b501801623a94e0db7EA12A40c74a1Ccd51A50"],
-    [100, 1000],
+    [1500, 30000],
     "dETF Token",
     "dETF"
 ];
